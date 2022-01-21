99f2e36af2b284de4da1dbdb7c1285d34bdf0021
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-22 00:00:05 +0900

    initial websocket setting (bithumb)

--- server/main.py
+++ server/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+import websockets
+import asyncio
 
 app = FastAPI()
 
 
+async def bithumb_ws_client():
+    uri = 'wss//pubwss.bithumb.com/pub/ws'
 
+    async with websockets.connect(uri) as websocket:
+        greeting = await websocket.recv()
+        print(greeting)
+
+
+async def main():
+    print('?')
+    await bithumb_ws_client()
+
+
+@app.get('/')
+async def test_get():
+    print('???????')
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
