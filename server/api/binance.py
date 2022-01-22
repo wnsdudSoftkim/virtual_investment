4896e2ede0ce12b366861c12c8c79e9475387aed
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-23 01:25:59 +0900

    store (2017-2022 coin data) into csv file

--- server/api/binance.py
+++ server/api/binance.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from fastapi import APIRouter
 from config.config import binance_ticker_uri, binance_symbol_uri
 import requests
+import pandas as pd
+from tqdm import tqdm
 
 router = APIRouter(
     prefix='/binance',
@@ -22,10 +24,17 @@ async def get_previous_data():
     start_date = '2021-06-01'
     end_date = '2021-12-31'
     symbols = symbols_usdit[0]
-    await _get_binance_data(start_date, end_date, symbols)
+    years = list(range(2017, 2022))
+    for symbol in symbols[:10]:
+        for year in years:
+            start_date = f'{year}-01-01'
+            end_date = f'{year}-12-31'
+            df = await _get_binance_data(start_date, end_date, symbols, COLUMNS)
+            df.to_csv(f'C:/Users/lenovo/CoinData/{symbol[:3].lower()}_{year}.csv', index=False)  # csv파일로 저장하는 부분
+            time.sleep(1)
 
 
-async def _get_binance_data(start_date, end_date, symbol):
+async def _get_binance_data(start_date, end_date, symbol, columns):
     data = []
     start = int(time.mktime(datetime.strptime(start_date + ' 00:00', '%Y-%m-%d %H:%M').timetuple())) * 1000
     end = int(time.mktime(datetime.strptime(end_date + ' 23:59', '%Y-%m-%d %H:%M').timetuple())) * 1000
@@ -44,4 +53,16 @@ async def _get_binance_data(start_date, end_date, symbol):
         js = result.json()
         if not js:
             break
-        print(js)
+        data.extend(js)
+        start = js[-1][0] + 60000
+        if not data:
+            print('no data')
+            return
+        df = pd.DataFrame(data)
+        df.columns = columns
+        df['Open_time'] = df.apply(lambda x: datetime.fromtimestamp(x['Open_time'] // 1000), axis=1)
+        df = df.drop(columns=['Close_time', 'ignore'])
+        df['Symbol'] = symbol
+        df.loc[:, 'Open':'tb_quote_av'] = df.loc[:, 'Open':'tb_quote_av'].astype(float)  # string to float
+        df['trades'] = df['trades'].astype(int)
+        return df
