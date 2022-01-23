c674ff1399088154f29fd6c0d7284230b270ac9d
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-23 11:43:19 +0900

    add vuex router

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import store from 'src/store/index'
+import router from 'src/router/index'
 createApp(App).mount('#app')
+
+
+async function init() {
+    const app = createApp(App)
+    app.use(store)
+    app.use(router)
+    app.mount('#app')
+}
+
+init()
