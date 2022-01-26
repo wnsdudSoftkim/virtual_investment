c0c94711d35557759d4fdb8f9e37a4aa79f8fad5
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-26 22:30:27 +0900

    setup several scss file

---
+++ client/src/components/layout/SectionLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="section-layout" >
+        <div class="section-container">
+            <slot></slot> <!--여기에 넣을 것 !-->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"section-layout",
+
+
+}
+</script>
+
+<style lang="scss">
+.section-layout{
+    padding:2rem;
+    padding-left:12rem;
+    padding-right:12rem;
+    .section-container{
+        width:100%;
+        height:40rem;
+        background: $secondColor;
+    }
+}
+</style>
