f6ef1ac40e7b32333d097684c4b378cb13456363
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-29 23:08:12 +0900

    modify mongodb connection, config setting

--- server/config/config.py
+++ server/config/config.py
@@ -5,3 +5,15 @@ binance_ticker_uri = 'https://api.binance.com/api/v3/ticker/price'
 binance_symbol_uri = 'https://api.binance.com/api/v3/klines'
 
 mongodb_service_local_uri = "mongodb://localhost:27017"
+
+
+# TODO: LOCAL이 아닌 배포시엔 ENV파일로 바꾸기
+class Settings:
+    PROJECT_NAME = "virtual-investment"
+    PROJECT_VERSION = "1.0.0"
+    COLLECTION_NAME: str = "investment"
+    MONGO_SERVICE_URI = "mongodb://localhost:27017"
+    MONGO_DB = "mydb"
+
+
+setting = Settings()
--- server/main.py
+++ server/main.py
@@ -1,23 +1,41 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from server.api import router
 from db import connect_db, close_db
+from config.config import settings
+
 app = FastAPI(openapi_url=None, docs_url=None, redoc_url=None)
 
 app.include_router(router)
 
 
-@app.get("/", include_in_schema=False)
-async def root():
-    return {
-        "running-mode": 'development'
-    }
+def include_router(app):
+    app.include_router(router)
 
 
-@app.on_event("startup")
-async def startup_evnet():
+#
+# def configure_static(app):
+#     app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def open_database_connection_pool():  # new
     connect_db()
 
 
-@app.on_event('shut_down')
-async def shutdown_event():
+def close_database_connection_pool():
     close_db()
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    open_database_connection_pool()
+    return app
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    close_database_connection_pool()
+
+
+app = start_application()
