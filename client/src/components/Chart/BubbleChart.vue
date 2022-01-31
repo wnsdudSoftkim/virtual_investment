0447a3f8b74d499281ef5972549943d23542c5ba
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-01-31 14:37:22 +0900

    add bubbleChart sample

--- client/src/components/Chart/BubbleChart.vue
+++ client/src/components/Chart/BubbleChart.vue
@@ -11,7 +11,7 @@ export default {
                 pointBackgroundColor: 'white',
                 borderWidth: 1,
                 pointBorderColor: '#24pEBF',
-                data:[{x: 100, y: 0, r: 10},{x: 60, y: 30, r: 20},{x: 40, y: 60, r: 25},{x: 80, y: 80, r: 50},]
+                data:[{x: 100, y: 0, r: 10},{x: 60, y: 30, r: 20},{x: 40, y: 60, r: 25},{x: 80, y: 80, r: 50},{x: 20, y: 30, r: 25},{x: 0, y: 100, r: 5}]
             }]
         },
         options: {
