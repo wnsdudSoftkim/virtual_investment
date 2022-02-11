0ead948e27771c157c041fcd1a8991c53a10cd09
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-02-11 20:25:12 +0900

    add ChartManager logic

--- server/api/chart.py
+++ server/api/chart.py
@@ -4,6 +4,8 @@ from db.model.chart import ChartInModel
 from db.operations import Operator as op
 from fastapi import APIRouter, WebSocket
 
+from logic.chart import ChartManager as cm
+
 router = APIRouter(
     prefix='/chart',
     tags=['chart']
@@ -13,40 +15,22 @@ router = APIRouter(
 @router.websocket('/ws')
 async def get_chart_endpoint(websocket: WebSocket):
     print('Accepting client connection...')
-    project = {'_id': 0, 'Close': 0, 'High': 0, 'Low': 0, 'Volume': 0, 'Open': 0, 'Open_time': 0, 'Symbol': 0,
-               'quote_av': 0, 'tb_base_av': 0, 'tb_quote_av': 0, 'trades': 0}
-    pipeline = [
-        {
-            '$addFields': {
-                'x': '$Open_time',
-                'y': '$Open'
-            }
-        },
-        {
-            '$match': {'Open_time': {'$gte': "2017-08-17 10:03:46.000Z"}}
-        }, {
-            '$limit': 30
-        }, {
-            '$project': project
-        },
+    pipeline = cm.get_pipeline()
 
-    ]
+    project = {'trades': 1, 'Volume': 1, 'Open': 1, '_id': 0}
     symbol = ""
     await websocket.accept()
     while True:
         try:
             print('prepare send')
             receive_txt = await receive_text(websocket)
-
             if receive_txt.date is not '1':
+                year = receive_txt.date[:4]
                 symbol = receive_txt.symbol if receive_txt.symbol is not None else symbol
                 pipeline[1]['$match']['Open_time']['$gte'] = receive_txt.date
-                res = await op.aggregate('b_2017', pipeline=pipeline)
-                other_res = await op.get('b_2017', {'Open_time': {'$gte': receive_txt.date}}, {'trades': 1, 'Volume': 1, 'Open': 1, '_id': 0})
-                result = {
-                    'res': res,
-                    'other_res': other_res
-                }
+                condition = cm.change_date(year)
+                result = await cm.get_chart_data(year, symbol, pipeline, condition, project)
+
                 await websocket.send_json(result)
                 print('send data')
 
@@ -58,5 +42,6 @@ async def get_chart_endpoint(websocket: WebSocket):
 
 async def receive_text(websocket: WebSocket) -> ChartInModel:
     res = await websocket.receive_json()
+    print(res)
     model = ChartInModel(**res)
     return model
---
+++ server/logic/chart.py
@@ -0,0 +1,50 @@
+from typing import List, Dict, Any
+from db.operations import Operator as op
+
+
+class ChartManager(object):
+
+    @classmethod
+    async def get_chart_data(cls, year: str, symbol: str, pipeline: List[Dict[str, Any]],
+                             condition: Dict[str, Any], project: Dict[str, int]):
+
+        res = await cls._get_price_data(year, symbol, pipeline)
+        other_res = await cls._get_trades_data(year, symbol, condition, project)
+        return {
+            'res': res,
+            'other_res': other_res
+        }
+
+    @classmethod
+    async def _get_price_data(cls, year: str, symbol: str, pipeline: List[Dict[str, Any]]):
+        return await op.aggregate(f'{symbol[0].lower()}_{year}', pipeline=pipeline)
+
+    @classmethod
+    async def _get_trades_data(cls, year: str, symbol: str, condition: Dict[str, Any], project: Dict[str, int]):
+        await op.get(f'{symbol[0]}_{year}', condition, project)
+
+    @classmethod
+    def change_date(cls, year: str):
+        return {'Open_time': {'$gte': year}}
+
+    @classmethod
+    def get_pipeline(cls):
+        project = {'_id': 0, 'Close': 0, 'High': 0, 'Low': 0, 'Volume': 0, 'Open': 0, 'Open_time': 0, 'Symbol': 0,
+                   'quote_av': 0, 'tb_base_av': 0, 'tb_quote_av': 0, 'trades': 0}
+        pipeline = [
+            {
+                '$addFields': {
+                    'x': '$Open_time',
+                    'y': '$Open'
+                }
+            },
+            {
+                '$match': {'Open_time': {'$gte': "2017-08-17 10:03:46.000Z"}}
+            }, {
+                '$limit': 30
+            }, {
+                '$project': project
+            },
+
+        ]
+        return pipeline
