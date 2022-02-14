639951b0211f33ab54f236d0c6b9b8a2ce36d978
wnsdudSoftkim <flgkselql98@gmail.com>
2022-02-14 23:22:34 +0900

    add quantity store vuex

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,9 +3,10 @@ import price from '@/store/price'
 import header from '@/store/header'
 import project from '@/store/project'
 import query from '@/store/query'
+import quantity from '@/store/quantity'
 
 const store =  createStore({
-    modules: {price, header, project, query}
+    modules: {price, header, project, query, quantity}
 })
 
 export default store
---
+++ client/src/store/quantity.js
@@ -0,0 +1,28 @@
+const quantity = {
+    state: () => ({ quantity: 0 }),
+    mutations: {
+      ASC_QUANTITY(state) {
+        state.quantity += 1;
+      },
+      DESC_QUANTITY(state) {
+          if (state === 0) alert('매도할 물량이 없습니다.')
+          state.quantity -= 1;
+      }
+    },
+    actions: {
+      descQuantity({commit}) {
+        commit('DESC_QUANTITY')
+      },
+      ascQuantity({commit}) {
+          commit('ASC_QUANTITY')
+      }
+
+    },
+    getters: {
+      updatequantity: state =>{
+        return state.quantity
+      }
+    }
+}
+
+export default quantity
