868b00c136c36bc9a8d3cf39b3fe6e4c225fa0c0
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-02-17 23:23:31 +0900

    move command file to command folder

---
+++ server/command/coin_1h.cmd
@@ -0,0 +1,10 @@
+@echo off
+set price_list=b_ c_ d_ s_ t_ u_
+set year_list=2017 2018 2019 2020 2021
+set value=_1h
+
+for %%a in (%price_list%) do (
+    for %%b in (%year_list%) do (
+        mongoimport --db mydb --collection %%a%%b --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\%%a%%b%value%.csv
+    )
+)
--- server/command/coin_1m.cmd
+++ server/command/coin_1m.cmd
@@ -1,21 +1,15 @@
 @echo off
 set price_list=b_ c_ d_ s_ t_ u_
 set year_list=2017 2018 2019 2020 2021
-
+set value=_1h
 @REM (for %%price in (%price_list%) do (
 @REM     (for %%year in (%year_list%) do (
 @REM         mongoimport --db mydb --collection %%price%%year --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\%%price%%year.csv
 @REM     )
 @REM ))
 @REM store 1m coin data
-@REM for %%a in (%price_list%) do (
-@REM     for %%b in (%year_list%) do (
-@REM         mongoimport --db mydb --collection %%a%%b --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\%%a%%b.csv
-@REM     )
-@REM )
-@REM store 1h coin data
 for %%a in (%price_list%) do (
     for %%b in (%year_list%) do (
-        mongoimport --db mydb --collection %%a%%b --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\%%a%%b_1h.csv
+        mongoimport --db mydb --collection %%a%%b --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\%%a%%b.csv
     )
 )
