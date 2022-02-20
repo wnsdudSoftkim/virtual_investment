929be53a25468ffc48d595760783df185c1377a9
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-02-20 17:04:48 +0900

    change mongodb (local -> cluster)

--- server/command/coin_30m.cmd
+++ server/command/coin_30m.cmd
@@ -2,9 +2,9 @@
 set price_list=btc_ eth_
 set year_list=2017 2018 2019 2020 2021
 set value=_1h
-
+set host="mongodb://flgkselql98:tjdnftkfka0501Q@investment-shard-00-00.9ea8t.mongodb.net:27017,investment-shard-00-01.9ea8t.mongodb.net:27017,investment-shard-00-02.9ea8t.mongodb.net:27017/Investment?ssl=true&replicaSet=atlas-9s1ro9-shard-0&authSource=admin&retryWrites=true&w=majority"
 for %%a in (%price_list%) do (
     for %%b in (%year_list%) do (
-        mongoimport --db mydb --collection %%a%%b_30m --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\30m\%%a%%b.csv
+        mongoimport --uri %host%  --collection %%a%%b_30m --type csv --headerline --ignoreBlanks --file C:\Users\lenovo\CoinData\30m\%%a%%b.csv
     )
 )
--- server/config/config.py
+++ server/config/config.py
@@ -12,8 +12,12 @@ class Settings:
     PROJECT_NAME = "virtual-investment"
     PROJECT_VERSION = "1.0.0"
     COLLECTION_NAME: str = "investment"
-    MONGO_SERVICE_URI = "mongodb://localhost:27017"
-    MONGO_DB = "mydb"
+    # MONGO_SERVICE_URI = "mongodb://localhost:27017"
+    MONGO_SERVICE_URI = "mongodb://flgkselql98:tjdnftkfka0501Q@investment-shard-00-00.9ea8t.mongodb.net:27017," \
+                        "investment-shard-00-01.9ea8t.mongodb.net:27017," \
+                        "investment-shard-00-02.9ea8t.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas" \
+                        "-9s1ro9-shard-0&authSource=admin&retryWrites=true&w=majority "
+    MONGO_DB = "investment"
 
 
 def get_settings():
